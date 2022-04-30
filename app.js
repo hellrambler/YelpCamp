61449041a42c418129ac7ee328e25670182851ac
hellrambler <91743399+hellrambler@users.noreply.github.com>
2022-04-30 15:34:40 -0400

    ready for deployment

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 if (process.env.NODE_ENV !== "productions"){
     require("dotenv").config();
 }
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 
+
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const session = require("express-session");
+const MongoStore  = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require('passport');
 const LocalStrategy = require("passport-local");
@@ -31,10 +34,10 @@ const reviewsRoutes = require("./routes/reviews");
 
 //user routes
 const usersRoutes = require("./routes/users");
-
+// 'mongodb://localhost:27017/yelp-camp'
 //connect to mongodb
 async function connect() {
-  await mongoose.connect('mongodb://localhost:27017/yelp-camp');
+  await mongoose.connect(dbUrl);
 }
 
 connect()
@@ -44,6 +47,7 @@ connect()
 
 const app = express();
 
+
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "views"));
@@ -51,6 +55,8 @@ app.set('views', path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+
+
 app.use(mongoSanitize());
 app.use(helmet({
     crossOriginEmbedderPolicy: false,
@@ -101,17 +107,23 @@ app.use(
     })
 );
 
+const secret = process.env.SECRET || "thisisasecret";
 
 const sessionConfig = {
     name: "session",
-    secret: "thisisasecret",
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7,
         httpOnly: true
-    }
+    },
+    store: MongoStore.create({ 
+        mongoUrl: dbUrl,
+        secret,
+        touchAfter: 24 * 3600
+     })
 };
 app.use(session(sessionConfig));
 app.use(flash());
